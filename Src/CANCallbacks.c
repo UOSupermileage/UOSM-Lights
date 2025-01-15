59c5bb5564be45ace483de314ebfeeff0759ab61
Jeremy <jeremymcote@gmail.com>
2025-01-14 21:24:02 -0500

    Cleanup CAN Callbacks

--- Src/CANCallbacks.c
+++ Src/CANCallbacks.c
@@ -1,55 +1,30 @@
 #include "ApplicationTypes.h"
 
 #include "CANMessageLookUpModule.h"
-//#include "DataAggregation.h"
-#include "InternalCommsModule.h"
 #include "CANDriver.h"
 #include "LightsDriver.h"
 
-void ErrorDataCallback(iCommsMessage_t *msg) {
-    DebugPrint("ErrorDataCallback! %d", msg->standardMessageID);
+void LightsDataCallback(iCommsMessage_t *msg){
+    uint32_t message = readMsg(msg);
+    setLightsStatus(message);
 }
 
 void EventDataCallback(iCommsMessage_t *msg) {
-    DebugPrint("EventDataCallback! %d", msg->standardMessageID);
     if (msg->dataLength == CANMessageLookUpTable[EVENT_DATA_ID].numberOfBytes) {
         EventCode code = msg->data[1];
         flag_status_t status = msg->data[0];
 
-        DebugPrint("EventDataCallback, received code %d with status %d", code, status);
         if(code == BRAKES_ENABLED) {
-            setBrakesStatus(Set);
-        }else{
-            setBrakesStatus(Clear);
+            setBrakesStatus(status);
         }
-    } else {
-        DebugPrint("msg.dataLength does not match lookup table. %d != %d", msg->dataLength, CANMessageLookUpTable[ERROR_DATA_ID].numberOfBytes);
     }
 }
 
-void MotorRPMDataCallback(iCommsMessage_t* msg) {
-    int32_t rpm = readMsg(msg);
-    DebugPrint("CAN rpm received: %d", rpm);
-}
-
-void CurrentVoltageDataCallback(iCommsMessage_t* msg) {
-
-    uint16_pair_t pair = readMsgPairUInt16Bit(msg);
-
-    DebugPrint("CAN current received: %d", pair.a);
-    DebugPrint("CAN voltage received: %d", pair.b);
-}
-
-void SpeedDataCallback(iCommsMessage_t *msg) {
-    uint32_t speed = readMsg(msg);
-    DebugPrint("CAN speed received: %d", speed);
-}
-
-void ThrottleDataCallback(iCommsMessage_t *msg) { DebugPrint("ThrottleDataCallback not implemented! %d", msg->standardMessageID); }
-void PressureDataCallback(iCommsMessage_t *msg) { DebugPrint("PressureDataCallback not implemented! %d", msg->standardMessageID); }
-void TemperatureDataCallback(iCommsMessage_t *msg) { DebugPrint("TemperatureDataCallback not implemented! %d", msg->standardMessageID); }
-void LightsDataCallback(iCommsMessage_t *msg){
-    uint32_t message = readMsg(msg);
-    setLightsStatus(message);
-    SerialPrintln("Received lights message!");
-}
+// Lights doesn't need most of the callbacks
+extern void ThrottleDataCallback(iCommsMessage_t *msg){}
+extern void ErrorDataCallback(iCommsMessage_t *msg){}
+extern void SpeedDataCallback(iCommsMessage_t *msg){}
+extern void MotorRPMDataCallback(iCommsMessage_t *msg){}
+extern void CurrentVoltageDataCallback(iCommsMessage_t *msg){}
+extern void PressureTemperatureDataCallback(iCommsMessage_t *msg){}
+extern void EfficiencyDataCallback(iCommsMessage_t *msg){}
