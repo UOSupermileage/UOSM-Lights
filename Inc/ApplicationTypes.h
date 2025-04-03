163997a57fa497e0c99de59febe19768097462df
Tristan Robichaud <contact@toxoscorp.com>
2025-04-02 20:24:57 -0400

    Squashed 'UOSM-Core/' changes from c819e0d..6065857
    6065857 Merge commit '0aba5200f6bc9307c71b342acdb82c076914141d'
    26a1f3c Fix light struct
    f867f37 Color picker
    946ee8e fix
    30d11aa Merge UOSM-Core
    33a3edf Merge temperature sensor into main
    d4b2182 Working temp sensor
    f2fcb16 Remove backup register code
    7b39f58 Redesing Dash / new SDL sim
    e6ce41f Update UOSM-Core
    32d5a22 Merge UOSM-Core
    944a683 Create motor temperature can message type.
    a6b4cb1 Update README.md
    9406134 Update README.md
    1ea30a8 Merge commit 'cf1d1e62ad026dfe96d3d22d207ac3099ae3efca'
    cb61314 Merge UOSM-Core
    b06b36d Enable RTC for backup registers, cleanup CAN code
    53ca664 Add speed dial
    60a325c All buttons assigned
    cfdb29e Simplify
    273c550 Merge commit '23c86a9a58ca8193b41700f8ad988d2a7b27dd3e'
    ceed32e Merge commit '9133adb7bf33efa816cd9a29e429307f5e6549d5'
    538017e Merge UOSM-Core
    58a1c50 Merge UOSM-Core
    
    git-subtree-dir: UOSM-Core
    git-subtree-split: 60658579b064421d37e5dbbf4a45df0eb5e0a152

--- Inc/ApplicationTypes.h
+++ Inc/ApplicationTypes.h
@@ -20,9 +20,6 @@ extern "C" {
 
 #include <stdint.h>
 
-#define PUBLIC
-#define PRIVATE
-
 #define PI_T2 6.28318530718 // Pi * 2
 
 #define MAX_PERCENTAGE 1000
@@ -44,17 +41,14 @@ typedef enum {
 } MotorCode;
 
 typedef enum {
-    THROTTLE_TOO_HIGH
-} ErrorCode;
-
-typedef enum {
     DEADMAN,
     EVENT_TIMER,
     MOTOR_INITIALIZING,
     UNDERVOLTAGE,
     DRIVER_ENABLED,
     NEW_LAP,
-    BRAKES_ENABLED
+    BRAKES_ENABLED,
+    THROTTLE_TOO_HIGH
 } EventCode;
 
 typedef enum {
@@ -81,7 +75,7 @@ typedef uint16_t current_t;
 typedef uint16_t watt_hour_t;
 typedef uint8_t brightness_t;
 typedef int32_t pressure_t;
-typedef int32_t temperature_t;
+typedef uint16_t temperature_t; // centi Celcius
 
 typedef struct {
     uint16_t standardMessageID; // 11 bit max
@@ -107,11 +101,15 @@ typedef struct {
 typedef union {
     uint32_t all;
     struct {
-        uint32_t hazards_enabled: 1;
-        uint32_t left_turn_enabled: 1;
-        uint32_t right_turn_enabled: 1;
-        uint32_t headlights_enabled: 1;
-        uint32_t low_beams_enabled: 28;
+        uint64_t hazards_enabled: 1;
+        uint64_t left_turn_enabled: 1;
+        uint64_t right_turn_enabled: 1;
+        uint64_t headlights_enabled: 1;
+        uint64_t low_beams_enabled: 1;
+        uint64_t r: 8;
+        uint64_t g: 8;
+        uint64_t b: 8;
+        uint64_t reserved: 3;
     };
 } lights_status_t;
 
